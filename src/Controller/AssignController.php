db2d5980a4f16acb07c9296d17c1b63f851d2860
Howard Noz <howardnoz@users.noreply.github.com>
2018-10-13 23:29:57 -0700

    Revert "Add centralized page for people and teams."
    This reverts commit f3338ca9f38e7142d6d1656748f7da6cd1e37c8d.

--- src/Controller/AssignController.php
+++ src/Controller/AssignController.php
@@ -4,21 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\PersonRepository;
-use App\Repository\TeamRepository;
-use Symfony\Component\HttpFoundation\Response;
 
 class AssignController extends AbstractController
 {
     /**
      * @Route("/assign", name="assign")
      */
-    public function index(PersonRepository $personRepository, TeamRepository $teamRepository): Response
+    public function index()
     {
         return $this->render('assign/index.html.twig', [
             'controller_name' => 'AssignController',
-            'people' => $personRepository->findAll(),
-            'teams' => $teamRepository->findAll(),
         ]);
     }
 }
